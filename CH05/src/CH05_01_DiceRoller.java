2637c8b1425acb36bed10585bb8c503f21433f1b
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-12-12 16:47:21 -0500

    practice

---
+++ CH05/src/CH05_01_DiceRoller.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CH05_01_DiceRoller {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dice Roller\n");
+		
+		String choice = "y";
+		
+		while (choice.equalsIgnoreCase("y")) {
+			
+			System.out.print("Roll the dice? (y/n): ");
+			choice = sc.next();
+			
+			// Logic
+			
+			 (int) (Math.random() * 6) + 1;
+			 
+			 if ()
+			
+			System.out.println("Die 1: " + die1);
+			System.out.println("Die 2: " + die2);
+			System.out.println("Total: " + sum);
+			
+			
+			
+			
+		}
+	}
+
+}
+
+
